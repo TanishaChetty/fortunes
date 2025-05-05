49cfaac891658fb8638c0b06ad92ff1f15b480e2
Lorelei Barnum <lbarnum@smith.edu>
2025-05-05 14:59:07 -0400

    Changed some things to better match lintr standards and to reduce errors

--- tests/testthat/test-fortunes-fundamentals.R
+++ tests/testthat/test-fortunes-fundamentals.R
@@ -2,9 +2,7 @@ test_that("the function returns a string", {
   expect_true(is.character(fortunes()))
 })
 
-test_that("the function throws an error when used with an input",{
+test_that("the function throws an error when used with an input", {
   expect_error(fortunes("string"))
 
 })
-
-
