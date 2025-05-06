52acb07ab84135e25aa3a26c0852ca375cae0431
Tanisha Chetty <tchetty@smith.edu>
2025-05-06 19:39:22 -0400

    fixed description and hopefully function...?

--- R/fortunes.R
+++ R/fortunes.R
@@ -1,5 +1,3 @@
-fortune_data <- NULL
-
 #' @title Fortune Teller
 #' @description
 #' This function has no function argument. Call the function and follow the interactive instructions to get a fortune.
@@ -15,28 +13,26 @@ fortune_data <- NULL
 fortunes <- function(fortuney_words = NULL, awesome_words = NULL, evil_words = NULL) {
 
   if (is.null(fortuney_words)){
-    fortuney_words = readline(prompt = "GET YOUR FORTUNE TOLD!!! Enter one of the following words: luck, treasure, omen, destiny... ")
-    if (!(fortuney_words %in% c("luck", "treasure", "omen", "destiny"))){
+    fortuney_words = readline(prompt = "GET YOUR FORTUNE TOLD!!! Enter one of the following words: luck, treasure, omen, destiny... ")}
+  if (!(fortuney_words %in% c("luck", "treasure", "omen", "destiny"))){
       stop("Woah! Try one of the words listed above maybe...")
-    }
   }
 
   if (is.null(awesome_words)){
-    awesome_words = readline(prompt = "Enter one of the following words: sparkle, unicorn, glitter, rainbow...")
-    if (!(awesome_words %in% c("sparkle", "unicorn", "glitter", "rainbow"))){
+    awesome_words = readline(prompt = "Enter one of the following words: sparkle, unicorn, glitter, rainbow...")}
+  if (!(awesome_words %in% c("sparkle", "unicorn", "glitter", "rainbow"))){
       stop("Woah! Try one of the words listed above maybe...")
-    }
+
   }
 
   if (is.null(evil_words)){
-    evil_words = readline(prompt = "Enter one of the following words:fire, brimstone, purgatory, damnation...")
-    if(!(evil_words %in% c( "fire","brimstone", "purgatory", "damnation"))){
+    evil_words = readline(prompt = "Enter one of the following words:fire, brimstone, purgatory, damnation...")}
+  if(!(evil_words %in% c( "fire","brimstone", "purgatory", "damnation"))){
       stop("Woah! Try one of the words listed above maybe...")
 
-    }
   }
 
-  if (str_length(var) %% 2 == 0) {
+  if (str_length(evil_words) %% 2 == 0) {
     x <- sample(1:25, size = 1)
     } else {
     x <- sample(26:50, size = 1)
